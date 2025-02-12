0c62f216112536eafa14b72e1db2fa7d05446522
Maheshgowdan <maheshgowdan911@gmail.com>
2025-02-12 13:54:55 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
-//def call(String name = 'mahesh') {
-  //echo "Hello, welcome ${name}"
-def call() {
-  echo "welcome, Mahesh"
+def call(String name = 'mahesh') {
+  echo "Hello, welcome ${name}"
+//def call() {
+  //echo "welcome, Mahesh"
 }
